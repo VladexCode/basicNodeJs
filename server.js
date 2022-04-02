1c5f786a1540d96e48f81fcf4ff696859b30a867
VladexCode <vladfernd@gmail.com>
2022-04-01 23:52:00 -0400

    contenido estatico

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World express')
-})
+//servir contenido estatico
+app.use(express.static("public"));
 
-app.listen(3000)
+app.get("/", function (req, res) {
+  res.send("Hello World express");
+});
+
+app.get("/nueva_ruta", (req, res) => {
+  res.send("nueva_ruta");
+});
+
+app.get("/segunda_ruta", (request, response) => {
+  response.send("segunda_ruta");
+});
+
+app.get("*", (req, res) => {
+    //la ruta debe ser absoluta
+    res.sendFile(__dirname+'/public/404.html');
+});
+
+app.listen(3000);
